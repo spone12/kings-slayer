9101dbdbb05080c7faa1998d4ebacf9a4b36e407
spone12 <spone185@gmail.com>
2022-12-14 21:09:03 +0300

    Move hero

--- kingSlayer.py
+++ kingSlayer.py
@@ -5,9 +5,14 @@ pygame.init()
 # Window game size
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption(NAME_GAME)
-pygame.display.set_icon(pygame.image.load("img/game/logo.bmp"))
+pygame.display.set_icon(pygame.image.load("img/Game/logo.bmp"))
 
 clock = pygame.time.Clock()
+
+player = pygame.image.load("img/Characters/mainHero.png")
+x = 50
+y = 50
+speed = 5
 
 while True:
     clock.tick(FPS)
@@ -15,3 +20,17 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+
+    controlKeys = pygame.key.get_pressed()
+
+    if controlKeys[pygame.K_LEFT]:
+        x -= speed
+    elif controlKeys[pygame.K_RIGHT]:
+        x += speed
+    elif controlKeys[pygame.K_UP]:
+        y -= speed
+    elif controlKeys[pygame.K_DOWN]:
+        y += speed
+
+    window.blit(player, (x, y))
+    pygame.display.update()
